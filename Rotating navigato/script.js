a661c06dee751d4e62ad87b68966a61107b57e1e
AmanSinghRajput <88625285+aman6969@users.noreply.github.com>
2023-01-28 12:22:38 +0530

    Add files via upload

---
+++ Rotating navigato/script.js
@@ -0,0 +1,5 @@
+const open=documnet.getElementById('open')
+const close=document.getElementById('close')
+const container = document.querySelector('.container')
+open.addEventlistener('click', () => container.classList.add('show-nav'))
+close.addEventlistener('click', () => container.classList.remove('show-nav'))
